8e14dfd8facd8d2e272692efa7f7b34653b7e509
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2024-10-22 23:59:27 +0900

    client

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,23 @@
-export const metadata = {
-  title: 'Home',
-};
+'use client';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [movies, setMovies] = useState();
+  const getMovies = async () => {
+    const response = await fetch(
+      'https://nomad-movies.nomadcoders.workers.dev/'
+    );
+    const json = await response.json();
+    setMovies(json);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <div>
       <h1>Home!</h1>
+      <div>{JSON.stringify(movies)}</div>
     </div>
   );
 }
