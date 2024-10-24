96ddb38c253bf53940b7a31ef45dd9fcb4c66ad8
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2024-10-25 03:09:17 +0900

    movie detail
    iframe 사용

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from '../app/(home)/page';
+import styles from '../styles/movie-info.module.css';
 
 async function getMovie(id: string) {
   const response = await fetch(`${API_URL}/${id}`);
@@ -6,6 +7,22 @@ async function getMovie(id: string) {
 }
 
 export default async function MovieInfo({ id }: { id: string }) {
-  const videos = await getMovie(id);
-  return <h6>{JSON.stringify(videos)}</h6>;
+  const movie = await getMovie(id);
+  return (
+    <div className={styles.container}>
+      <img
+        src={movie.poster_path}
+        className={styles.poster}
+        alt={movie.title}
+      />
+      <div className={styles.info}>
+        <h1 className={styles.title}>{movie.title}</h1>
+        <h3>⭐️ {movie.vote_average.toFixed(1)}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target={'_blank'}>
+          Homepage &rarr;
+        </a>
+      </div>
+    </div>
+  );
 }
