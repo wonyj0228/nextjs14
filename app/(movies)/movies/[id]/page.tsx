80dbedb8895c706501a06014382b90490ab86bf6
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2024-10-26 00:00:16 +0900

    dynamic metadata

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,12 +1,19 @@
 import { Suspense } from 'react';
-import MovieInfo from '../../../../components/movie-info';
+import MovieInfo, { getMovie } from '../../../../components/movie-info';
 import MovieVideos from '../../../../components/movie-videos';
 
-export default function MovieDetail({
-  params: { id },
-}: {
+interface IParams {
   params: { id: string };
-}) {
+}
+
+export async function generateMetadata({ params: { id } }: IParams) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
+
+export default function MovieDetail({ params: { id } }: IParams) {
   return (
     <div>
       <h3>Movie detail page</h3>
